dd662397ac8e20ca3e2b198f5945f54b20b8a032
pedrao123 <pedroavidago174@outlook.com>
2025-02-14 20:43:16 -0300

    adicionar structs

--- main.c
+++ main.c
@@ -3,6 +3,30 @@
 #include<string.h>
 int main()
 {
+    typedef struct
+    {
+        int id;
+        char *nome ;
+        char *especialidade;
+    } medico;
+    typedef struct
+    {
+        int id;
+        char *nome;
+        char *identidade;
+        char *endereco;
+        char *telefone;
+        char *sexo
+    }paciente;
+    typedef struct
+    {
+        int numero;
+        int idMedico;
+        int idPaciente;
+        char *horario;
+        char *duracao;
+        char *data;
+    }consulta;
     
     return 0;
 }
