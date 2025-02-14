52842a3209b63a80187cadbf56717217e772e639
pedrao123 <pedroavidago174@outlook.com>
2025-02-14 20:56:57 -0300

    atualizar structs, inicializador dos arquivos

--- main.c
+++ main.c
@@ -1,32 +1,59 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-int main()
-{
-    typedef struct
+#define MaxCaracteres 40
+typedef struct
     {
         int id;
-        char *nome ;
-        char *especialidade;
+        char nome[MaxCaracteres] ;
+        char especialidade[MaxCaracteres];
     } medico;
     typedef struct
     {
         int id;
-        char *nome;
-        char *identidade;
-        char *endereco;
-        char *telefone;
-        char *sexo
+        char nome[MaxCaracteres];
+        char identidade[MaxCaracteres];
+        char endereco[MaxCaracteres];
+        char telefone[MaxCaracteres];
+        char sexo[MaxCaracteres];
     }paciente;
     typedef struct
     {
         int numero;
         int idMedico;
         int idPaciente;
-        char *horario;
-        char *duracao;
-        char *data;
+        char horario[MaxCaracteres];
+        char duracao[MaxCaracteres];
+        char data[MaxCaracteres];
     }consulta;
+int main()
+{
+    FILE *arqmed = fopen("medicos.bin","r");
+    if(arqmed==NULL){
+        FILE *arqmed = fopen("medicos.bin","w");
+        fclose(arqmed);
+    }
+    else{
+        fclose(arqmed);
+    }
+
+    FILE *arqpac = fopen("pacientes.bin","r");
+    if(arqmed==NULL){
+        FILE *arqpac = fopen("pacientes.bin","w");
+        fclose(arqpac);
+    }
+    else{
+        fclose(arqpac);
+    }
+
+    FILE *arqcons = fopen("consultas.bin","r");
+    if(arqcons==NULL){
+        FILE *arqcons = fopen("cansultas.bin","w");
+        fclose(arqcons);
+    }
+    else{
+        fclose(arqcons);
+    }
     
     return 0;
 }
